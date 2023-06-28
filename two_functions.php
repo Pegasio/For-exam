b31580a1c2a701bdd21be1d8146f6a95649c6dd4
Pegasio <109171075+Pegasio@users.noreply.github.com>
2023-06-28 23:04:14 +0400

    Create two_functions.php

---
+++ two_functions.php
@@ -0,0 +1,39 @@
+// Создаем дополнительные типы записей
+function custom_post_types() {
+    // Создаем тип записи для сотрудников
+  register_post_type('employee', array(
+      'labels' => array(
+          'name' => 'Сотрудники',
+          'singular_name' => 'Сотрудник'
+      ),
+      'public' => true,
+      'has_archive' => true,
+  ));
+
+register_post_type('services', array(
+   'labels' => array(
+      'name' => 'Услуги',
+      'singular_name' => 'Услуга'
+),
+   'public' => true,
+   'has_archive' => true,
+));
+   
+}
+
+// Остальные типы создаются тем же образом
+add_action('init', 'custom_post_types');
+
+// Создаем таксономию для категорий сотрудников
+function custom_taxonomy() {
+    register_taxonomy(
+        'employee_category',
+        'employee',
+        array(
+            'label' => 'Категории сотрудников',
+            'rewrite' => array('slug' => 'employee-category'),
+            'hierarchical' => true,
+        )
+    );
+}
+add_action('init', 'custom_taxonomy');"
